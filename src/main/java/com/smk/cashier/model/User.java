e66389bf18ec95b2f09f26177a6c7d6037d6507a
kimiginting <kimiginting@hotmail.com>
2023-08-29 21:52:33 +0700

    create user and model class

--- src/main/java/com/smk/cashier/model/User.java
+++ src/main/java/com/smk/cashier/model/User.java
@@ -1,7 +1,7 @@
 package com.smk.cashier.model;
 
 public class User extends Model {
-    private String userName;
+    private String username;
     private String password;
 
     public String getUsername() {
