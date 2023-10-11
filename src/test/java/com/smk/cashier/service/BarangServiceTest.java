da12f2000f25d096df39a49852ba3ae02ac3cf95
kimiginting <kimiginting@hotmail.com>
2023-10-11 14:33:40 +0700

    Adding and init DAO

--- src/test/java/com/smk/cashier/service/BarangServiceTest.java
+++ src/test/java/com/smk/cashier/service/BarangServiceTest.java
@@ -1,11 +1,13 @@
 package com.smk.cashier.service;
 
+import com.smk.cashier.dao.BarangDao;
 import com.smk.cashier.model.Barang;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +27,36 @@ class BarangServiceTest {
     void findByName() {
         List<Barang> resultList = BarangService.getInstance().findByName("Laptop Gaming");
         assertEquals(resultList.size(),2);
+    }
+
+    @Test
+    @Order(4)
+    void saveBarangToDatabase() {
+        BarangDao barangDao = new BarangDao();
+        Barang laptop = new Barang();
+        laptop.setKodeBarang("LP001");
+        laptop.setNamaBarang("Laptop");
+        laptop.setHargaBarang(5000000);
+        laptop.setDateCreated(new Date());
+        laptop.setLastModified(new Date());
+        barangDao.save(laptop);
+
+        Barang mouse = new Barang();
+        mouse.setKodeBarang("M0001");
+        mouse.setNamaBarang("Mouse");
+        mouse.setHargaBarang(100000);
+        mouse.setDateCreated(new Date());
+        mouse.setLastModified(new Date());
+        barangDao.save(mouse);
+
+        Barang laptopGaming = new Barang();
+        laptopGaming.setKodeBarang("LP002");
+        laptopGaming.setNamaBarang("Laptop Gaming");
+        laptopGaming.setHargaBarang(20000000);
+        laptopGaming.setDateCreated(new Date());
+        laptopGaming.setLastModified(new Date());
+        barangDao.save(laptopGaming);
+
     }
 
     @Test
